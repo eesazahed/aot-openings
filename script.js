9cec97d1f7ae19ec4c285f288969b0505e4a7eac
Eesa Zahed <eszhd1@gmail.com>
2024-08-10 14:44:23 -0400

    added more audios

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const button = document.querySelector("#play");
-const nowPlaying = document.querySelector("#now-playing");
+const nowPlaying = document.getElementById("now-playing");
+const songList = document.getElementById("song-list");
+const playPauseBtn = document.getElementById("play-pause-btn");
 
 let currentAudio = null;
+let currentAudioId = null;
+let isPlaying = false;
 
-const audios = {
-  1: "Guren no Yumiya",
-};
+const audios = ["Guren no Yumiya", "Jiyuu no Tsubasa", "Shinzou wo Sasageyo"];
 
-button.addEventListener("click", () => {
+audios.forEach((audioName) => {
+  const songId = audios.indexOf(audioName);
+
+  let songListItem = document.createElement("p");
+  songListItem.innerText = audioName;
+  songListItem.setAttribute("class", "song-list-item");
+  songListItem.onclick = () => playSong(songId);
+  songList.append(songListItem);
+});
+
+const playSong = (songId) => {
   if (nowPlaying.style.opacity !== 1) {
     nowPlaying.style.opacity = 1;
   }
@@ -17,9 +28,34 @@ button.addEventListener("click", () => {
     currentAudio.currentTime = 0;
   }
 
-  const audioId = 1;
-
-  nowPlaying.innerText = `OP ${1}: ${audios[audioId]}`;
-  currentAudio = new Audio(`./assets/sounds/${audioId}.mp3`);
+  nowPlaying.innerText = `OP ${songId + 1}: ${audios[songId]}`;
+  currentAudio = new Audio(`./assets/sounds/${songId}.mp3`);
+  currentAudioId = songId;
   currentAudio.play();
-});
+
+  currentAudio.onended = () => {
+    const nextSongId = songId % audios.length;
+    playSong(nextSongId);
+  };
+};
+
+const togglePlayPause = () => {
+  if (!currentAudio) {
+    playSong(0);
+  }
+
+  if (isPlaying) {
+    playPauseBtn.style.background = "green";
+
+    currentAudio.pause();
+    playPauseBtn.innerText = "Play";
+  } else {
+    playPauseBtn.style.background = "red";
+    currentAudio.play();
+    playPauseBtn.innerText = "Pause";
+  }
+
+  isPlaying = !isPlaying;
+};
+
+playPauseBtn.addEventListener("click", togglePlayPause);
